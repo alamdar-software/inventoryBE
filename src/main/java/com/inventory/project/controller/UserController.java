9e6afbacd063acad242c736d3ac5567dcccb29dd
MehranJamal <114913529+Mehran-Jamal@users.noreply.github.com>
2024-02-12 16:00:11 +0530

    add

--- src/main/java/com/inventory/project/controller/UserController.java
+++ src/main/java/com/inventory/project/controller/UserController.java
@@ -1,13 +1,36 @@
 package com.inventory.project.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.inventory.project.model.User;
+import com.inventory.project.payload.request.AddUser;
+import com.inventory.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+    @Autowired
+    private UserRepository userRepository;
 
+    // View details of a specific user by ID
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUserById(@PathVariable Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return ResponseEntity.ok(user);
+    }
+
+    // View details of all users
+    @GetMapping("/view")
+    @PreAuthorize("hasRole('SUPERADMIN')")
+    public ResponseEntity<?> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
 }
